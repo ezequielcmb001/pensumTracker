164fdc19190f02cb3b4d27dbcb6db4efde4b9829
ezequielmcb <ezequielcespede@gmail.com>
2025-07-09 16:11:49 -0400

    .

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -7,7 +7,7 @@ export default function Login({ onLoginSuccess }: { onLoginSuccess: () => void }
         <View className="flex-1 items-center justify-center bg-white">
             <Text className="text-xl font-bold mb-4">Login</Text>
             <TouchableOpacity onPress={onLoginSuccess} className="bg-blue-500 px-4 py-2 rounded">
-                <Text className="text-white">Entrar</Text>
+                <Text className="text-white">Enter login</Text>
             </TouchableOpacity>
         </View>
     </SafeAreaView>
