2778c5aacd43b708f0ae51f79dde9cb3faf1e446
ezequielmcb <ezequielcespede@gmail.com>
2025-07-13 13:21:13 -0400

    .

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { Slot } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+  
   return (
     <SafeAreaProvider>
       <Slot />
